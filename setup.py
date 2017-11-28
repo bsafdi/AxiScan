c5cbbf995e52d40fd8ff88b4b4f9811e630b5f54
bsafdi <basfdi@mit.edu>
2017-11-27 20:44:42 -0500

    setup setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,19 @@ import numpy
 import cython_gsl
 
 extensions = [
-    Extension("AxiScan.*", ["AxiScan/*.pyx"],
+    Extension("AxiScan.axion_ll", ["AxiScan/axion_ll.pyx"],
+              include_dirs=[numpy.get_include(),cython_gsl.get_include()], library_dirs=[cython_gsl.get_library_dir()],libraries=["m","gsl","gslcblas"],
+              extra_compile_args = ["-O3", "-ffast-math", "-march=native", "-fopenmp","-lcblas","-lgslcblas","-lgsl"],
+              extra_link_args=['-fopenmp']),
+    Extension("AxiScan.mc_gen", ["AxiScan/mc_gen.pyx"],
+              include_dirs=[numpy.get_include(),cython_gsl.get_include()], library_dirs=[cython_gsl.get_library_dir()],libraries=["m","gsl","gslcblas"],
+              extra_compile_args = ["-O3", "-ffast-math", "-march=native", "-fopenmp","-lcblas","-lgslcblas","-lgsl"],
+              extra_link_args=['-fopenmp']),
+    Extension("AxiScan.scan", ["AxiScan/scan.pyx"],
+              include_dirs=[numpy.get_include(),cython_gsl.get_include()], library_dirs=[cython_gsl.get_library_dir()],libraries=["m","gsl","gslcblas"],
+              extra_compile_args = ["-O3", "-ffast-math", "-march=native", "-fopenmp","-lcblas","-lgslcblas","-lgsl"],
+              extra_link_args=['-fopenmp']),
+    Extension("AxiScan.speed_dist", ["AxiScan/speed_dist.pyx"],
               include_dirs=[numpy.get_include(),cython_gsl.get_include()], library_dirs=[cython_gsl.get_library_dir()],libraries=["m","gsl","gslcblas"],
               extra_compile_args = ["-O3", "-ffast-math", "-march=native", "-fopenmp","-lcblas","-lgslcblas","-lgsl"],
               extra_link_args=['-fopenmp'])
@@ -35,4 +47,5 @@ setup(packages=['AxiScan'],
     ext_modules = cythonize(extensions),
     **setup_args
 )
+
 print("Compilation successful!")
